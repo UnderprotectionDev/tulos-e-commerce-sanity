f3d21e618fd718dd289ff33eec8a15a878fcd57b
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-03-03 18:17:05 +0300

    Refactor home tabbar component with minor styling and syntax improvements

--- components/home-tabbar.tsx
+++ components/home-tabbar.tsx
@@ -10,18 +10,18 @@ export function HomeTabbar({ selectedTab, onTabSelect }: HomeTabbarProps) {
   return (
     <div className="flex items-center gap-1.5 text-sm font-semibold">
       <div className="flex items-center gap-1.5">
-        {productType?.map((item) => (
+        {productType.map((item) => (
           <button
-            key={item?.title}
-            onClick={() => onTabSelect(item?.title)}
-            className={`border border-[var(--dark-color)] px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-[var(--dark-color)] hover:text-white hoverEffect ${selectedTab === item?.title && "bg-[var(--dark-color)] text-white"}`}
+            key={item.title}
+            onClick={() => onTabSelect(item.title)}
+            className={`border border-[var(--dark-color)] px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-[var(--dark-color)] hover:text-white hoverEffect ${selectedTab === item.title && "bg-[var(--dark-color)] text-white"}`}
           >
             {item?.title}
           </button>
         ))}
       </div>
-      <button className="border border-darkColor p-2 rounded-full hover:bg-darkColor hover:text-white hoverEffect">
-        <Repeat className="w-5 h-5" />
+      <button className="border border-[var(--dark-color)] p-2 rounded-full hover:bg-[var(--dark-color)] hover:text-white hoverEffect">
+        <Repeat className="size-5" />
       </button>
     </div>
   );
